9bc46b2f77a68a42b2ea171fb71f9e6955299aff
marcos <marcos.florencio@tecnospeed.com.br>
2018-09-05 17:15:04 +0000

    update example

--- links.js
+++ links.js
@@ -1,3 +1,10 @@
 module.exports = {
+  github: 'https://github.com/eptaccio',
+  twitter: 'https://twitter.com/eptaccio',
+  linkedin: 'https://www.linkedin.com/in/eptaccio',
+  facebook: 'https://fb.com/eptaccio',
+  telegram: 'https://t.me/eptaccio',
+  email: 'eptaccio@gmail.com',
+  medium: 'https://medium.com/@eptaccio',
   example: 'http://pudim.com.br'
 }
